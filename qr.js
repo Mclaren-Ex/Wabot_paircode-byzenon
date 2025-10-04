a00ca601cc280a8ef1d8acbc373c7975366bd897
Mclaren-Ex <mclarenololade1@gmail.com>
2025-10-04 14:19:35 +0100

    Update qr.js

--- qr.js
+++ qr.js
@@ -70,7 +70,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "malvin~" + string_session;
+                        let md = "kitan~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `Hey there, KITAN XMD User!* 👋🏻
 
